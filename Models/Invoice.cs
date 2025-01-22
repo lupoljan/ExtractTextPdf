f2cecfad21d2ba2ccabe5e989ba90756272375bb
lupoljan <lupoljan@o2.pl>
2025-01-23 00:07:36 +0100

    load csv

--- Models/Invoice.cs
+++ Models/Invoice.cs
@@ -1,8 +1,12 @@
-﻿namespace ExtractTextPdf.Models
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace ExtractTextPdf.Models
 {
     public class Invoice
     {
+        [Name("Numer faktury")]
         public string InvoiceNumber { get; set; }
+        [Name("Kwota")]
         public int Price { get; set; }
     }
 
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using CsvHelper.Configuration;
+using CsvHelper;
 using ExtractTextPdf.Models;
 using Microsoft.Win32;
 using Syncfusion.Pdf.Interactive;
 using Syncfusion.Windows.PdfViewer;
+using System.Globalization;
+using System.IO;
 using System.Windows;
 
 namespace ExtractTextPdf.ViewModels
@@ -41,15 +45,31 @@ namespace ExtractTextPdf.ViewModels
         }
         public bool HasPdfFiles => PdfFiles != null && PdfFiles.Any();
 
+        private List<Invoice> _csvData;
+        public List<Invoice> CsvData
+        {
+            get => _csvData;
+            set
+            {
+                SetProperty(ref _csvData, value);
+                OnPropertyChanged(nameof(HasCsvData));
+            }
+        }
+
+        public bool HasCsvData => CsvData != null && CsvData.Any();
+
         public IRelayCommand LoadPdfCommand { get; }
         public IRelayCommand ClearPdfListCommand { get; }
+        public IRelayCommand LoadCsvCommand { get; }
 
         public MainViewModel()
         {
             PdfFiles = new List<string>();
+            CsvData = new List<Invoice>();
 
             LoadPdfCommand = new RelayCommand(LoadPdf);
             ClearPdfListCommand = new RelayCommand(ClearPdfList);
+            LoadCsvCommand = new RelayCommand(LoadCsv);
         }
 
         private void LoadPdf()
@@ -66,10 +86,39 @@ namespace ExtractTextPdf.ViewModels
                 MessageBox.Show($"Loaded {PdfFiles.Count} PDF files.");
             }
         }
+
         private void ClearPdfList()
         {
             PdfFiles.Clear();
             PdfFiles = new List<string>();
+        }
+
+        private void LoadCsv()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "CSV files (*.csv)|*.csv"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string csvPath = openFileDialog.FileName;
+
+                try
+                {
+                    using (var reader = new StreamReader(csvPath))
+                    using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
+                    {
+                        CsvData = csv.GetRecords<Invoice>().ToList();
+                    }
+
+                    MessageBox.Show($"Loaded {CsvData.Count} invoices from CSV file.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error reading CSV file: {ex.Message}");
+                }
+            }
         }
 
     }
